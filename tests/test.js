0489a715e4b3354920f73d38903b40e27caddc1c
Siriwan Limsuwan <60160316@go.buu.ac.th>
2020-11-07 17:17:42 +0700

    First Commit

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+const Request = require('supertest')
+const app = require('../index')
+
+describe('Validate setting the test', () => {
+    it('should be ok', () => {
+        assert.ok(true)
+    })
+})
+
+describe('GET "/"',()=>{
+    it('respond with Hello!',(done) =>{
+        Request(app).get('/').expect('Hello!',done)
+
+    })
+})

d4da9e35128bb48f232df7bd722b40dc9f4ec085
Siriwan Limsuwan <60160316@go.buu.ac.th>
2020-11-07 17:57:48 +0700

    add unit-test

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const item = [
+    { id:1,name:'iphone12' },
+    { id:2,name:'nokia3301' }
+]
 
 app.get('/', (req, res) => {
     return res.send('Hello!')
+})
+
+app.get('/item', (req, res) => {
+    return res.send(item)
 })
 
 app.listen(port, () => {
